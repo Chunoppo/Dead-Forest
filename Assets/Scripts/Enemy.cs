edc7236d54e6e309f1b2fb08d3ca2a2e48eddbc6
DuongPham2005 <duongptps40672@gmail.com>
2024-11-29 13:53:21 +0700

    update 29/11
    game mot nguoi

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,6 +11,17 @@ public class Enemy : MonoBehaviour
     public float radius = 10f;
     public Vector3 originalePosition;
     public float maxDistance = 50f;
+
+    public Animator animator;
+    // Update is called once per frame
+
+    public enum CharacterState
+    {
+        Normal,
+        Attack,
+        Die
+    }
+    public CharacterState currentState;
     void Start()
     {
         originalePosition = transform.position;
@@ -21,11 +32,47 @@ public class Enemy : MonoBehaviour
         var distance = Vector3.Distance(target.position, transform.position);
         if (distance <= radius && distanceToOriginal <= maxDistance)
         {
-            navMeshAgent.SetDestination(target.position);   
+            navMeshAgent.SetDestination(target.position);
+            animator.SetFloat("speed", navMeshAgent.velocity.magnitude);
+
+            distance = Vector3.Distance(target.position, transform.position);
+            if (distance < 2f)
+            {
+                ChangeState(CharacterState.Attack);
+            }
         }
         if (distance > radius || distanceToOriginal > maxDistance)
         {
             navMeshAgent.SetDestination(originalePosition);
+            animator.SetFloat("speed", navMeshAgent.velocity.magnitude);
+
+            distance = Vector3.Distance(originalePosition, transform.position);
+            if(distance < 1f)
+            {
+                animator.SetFloat("speed", 0);
+            }
+            ChangeState(CharacterState.Normal);
         }
     }
+    private void ChangeState(CharacterState newState)
+    {
+        switch (currentState)
+        {
+            case CharacterState.Normal:
+                break;
+            case CharacterState.Attack:
+                break;
+        }
+
+        switch (newState)
+        {
+            case CharacterState.Normal:
+                break;
+            case CharacterState.Attack:
+                animator.SetTrigger("Attack");
+                break;
+        }
+        currentState = newState;
+    }
+
 }
